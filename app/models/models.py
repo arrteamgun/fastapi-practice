fae1c65cf96026e8f0cbeb52c1636163e3474bb4
Roman Timganov <romatimganov@gmail.com>
2024-03-06 19:41:49 +0400

    changed the logic of checking the age

--- app/models/models.py
+++ app/models/models.py
@@ -3,9 +3,3 @@ from pydantic import BaseModel, computed_field
 class User(BaseModel):
     name: str
     age: int
-
-class UserAgeResponse(User, BaseModel):
-    @computed_field
-    @property
-    def is_adult(self) -> bool:
-        return self.age >= 18
