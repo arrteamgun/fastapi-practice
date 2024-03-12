71065499ad5be9c8ed9af43b4b632901be6c98c3
Roman Timganov <romatimganov@gmail.com>
2024-03-12 17:30:24 +0400

    added cookies for authorization

--- app/main.py
+++ app/main.py
@@ -1,25 +1,27 @@
-from fastapi import FastAPI
-from .models.models import Product
-from .models.sample import sample_products
+from fastapi import FastAPI, Response, Cookie
+from datetime import datetime
+from .models.models import User
 
 app = FastAPI()
 
-lst = []
+sample_user: dict = {"username": "Vanya", "password": "123esad"}
+sessions: dict = {}
+fake_db: list[User] = [User(**sample_user)]
 
 
-@app.get("/product/search")
-def search(keyword: str, category: str = None, limit: int = 10):
-
-    result = list(filter(lambda item: keyword.lower()
-                  in item['name'].lower(), sample_products))
-    print(result)
-    if category:
-        result = list(
-            filter(lambda item: item["category"] == category, result))
-
-    return result[:limit]
+@app.post("/login")
+async def login(usr: User, response: Response):
+    for u in fake_db:
+        if u.username == usr.username and u.password == usr.password:
+            session_token = "sesstok12"
+            sessions[session_token] = usr
+            return {"message": "куки установлены"}
+    return {"message": "Invalid username or password"}
 
 
-@app.get("/product/{product_id}")
-async def get_product_by_id(product_id: int) -> Product:
-    return [i for i in sample_products if i["product_id"] == product_id][0]
+@app.get('/user')
+async def user_info(session_token=Cookie()):
+    user = sessions.get(session_token)
+    if user:
+        return user.dict()
+    return {"message": "Unauthorized"}
--- app/models/models.py
+++ app/models/models.py
@@ -1,8 +1,6 @@
 from pydantic import BaseModel
 
 
-class Product(BaseModel):
-    product_id: int
-    name: str
-    category: str
-    price: float
+class User(BaseModel):
+    username: str
+    password: str
